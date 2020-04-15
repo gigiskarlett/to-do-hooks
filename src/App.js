42540906bd2d067b801e493f8e4b2f5ef8b51d7a
gigi <gigi@forethought.ai>
2020-04-15 16:18:45 -0700

    to-do with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [list, setList] = useState([{ id: "5", value: "Buy groceries" }]);
+	const [item, setItem] = useState("");
+
+	const addItem = () => {
+		const newItem = {
+			id: 1 + Math.random(),
+			value: item.slice(),
+		};
+
+		let newList = [...list];
+		newList.push(newItem);
+		setList(newList);
+		setItem("");
+	};
+
+	const updateItem = (key, value) => {
+		setItem(value);
+	};
+
+	return (
+		<div className="App">
+			<input
+				type="text"
+				placeholder="Type here..."
+				value={item}
+				onChange={(e) => updateItem("item", e.target.value)}
+			/>
+			<button onClick={() => addItem()}>Input</button>
+			<ul>
+				{list.map((item, index) => (
+					<li key={item.id}>{item.value}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
